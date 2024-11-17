e8faefefad51fe299910e433ba4bf7d37e81051b
Maiko <maikokurai@gmail.com>
2024-11-17 22:11:44 +0000

    Dawntrail

--- src/components/Combatant/index.js
+++ src/components/Combatant/index.js
@@ -22,6 +22,7 @@ const jobs = [
     'mnk',
     'mrd',
     'nin',
+    'pct',
     'pld',
     'pgl',
     'rdm',
@@ -32,6 +33,7 @@ const jobs = [
     'sge',
     'smn',
     'thm',
+    'vpr',
     'war',
     'whm',
 ];
